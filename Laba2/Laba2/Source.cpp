9e9c69e1faaa58de746117aa40e8a9b7d9bb2b21
Denis Shafarenko <phisika001@mail.ru>
2023-10-30 21:43:51 +0300

    done

--- Laba2/Laba2/Source.cpp
+++ Laba2/Laba2/Source.cpp
@@ -1,36 +1,53 @@
 #include <Windows.h>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <codecvt>
+
+#define ON_OPEN_FILE   1
+
+#define ID_EDIT_LINES       1001
+#define ID_EDIT_ROWS        1002
+#define ID_STATIC_LINES     1003
+#define ID_STATIC_ROWS      1004
+#define ID_STATIC_TEXT      1005
+#define ID_EDIT_TEXT        1006
 
 #define MAX_LINES           15
 #define MAX_ROWS            15
 #define MAX_TEXTLEN         41
-#define TOOL_HIGHT          30
+#define TOOL_BAR_HIGHT       0
+#define MAX_FILE_NAME      260 
 
 const std::wstring startText = L"Привет мир!!!";
 const int textHight = 50;
-
-int wndH = 700, wndW = 1200;
-int nLines = 7, mRows = 7;
 int cellH, cellW;
-std::wstring textTable[MAX_LINES][MAX_ROWS];
+const RECT constRect = { 0, 30, 0 + 50, 30 + 50 };
+
+int wndH = 600, wndW = 800;
+int nRows = 1, mColumns = 1;
+HINSTANCE hInstance, hLines, hRows, hText;
+std::vector<std::wstring> text_table {L""};
+
+OPENFILENAME ofn;
+wchar_t filename[MAX_FILE_NAME];
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 BOOL DrawLine(HDC hdc, int x1, int y1, int x2, int y2);
 void RecalculateTable();
+void FindBiggestCell(HDC hdc, std::vector<std::wstring> textTable, int nRows, int mColumns, POINT &posMaxH, POINT &posMaxW, LOGFONT &lf);
+void DisplayText(HDC hdc, std::vector<std::wstring> textTable, int nRows, int mColumns);
+void DisplayTableLines(HDC hdc);
+void InitializeMenu(HWND hwnd);
+void InitializeOFN(HWND hwnd);
+int LoadData(wchar_t filename[MAX_FILE_NAME], std::vector<std::wstring> textTable);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE _hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     HWND hWnd; MSG msg;
     WNDCLASSEX wcexmain;
 
-
-    for (int i = 0; i < MAX_LINES; i++)
-    {
-        for (int j = 0; j < MAX_ROWS; j++)
-        {
-            textTable[i][j] = startText;
-        }
-    }
+    hInstance = _hInstance;
 
     wcexmain.cbSize = sizeof(WNDCLASSEX);
     wcexmain.style = 0;
@@ -73,6 +90,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+    case WM_CREATE:
+        InitializeMenu(hWnd);
+        InitializeOFN(hWnd);
+        break;
+    case WM_COMMAND:
+        switch (wParam)
+        {
+        case ON_OPEN_FILE:
+            if (GetOpenFileName(&ofn))
+                LoadData(filename, text_table);
+                RecalculateTable();
+                InvalidateRect(hWnd, NULL, TRUE);
+            break;
+        }
+        break;
     case WM_SIZE:
     {
         wndH = HIWORD(lParam);
@@ -90,115 +122,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         LOGFONT lf;
         TEXTMETRIC tm;
         RECT rect;
-        int x, y = TOOL_HIGHT;
-        POINT posMaxH = { 0, 0 };
-        POINT posMaxW = { 0, 0 };
-        int maxW = 0;
-        int maxH = 0;
-        const RECT constRect = { 0, 30, 0 + cellW, 30 + cellH };
+        POINT posMaxH {};
+        POINT posMaxW {};
+        
 
-        memset(&lf, 0, sizeof(lf));
+        ZeroMemory(&lf, sizeof(lf));
         lf.lfPitchAndFamily = FF_MODERN | DEFAULT_PITCH;
         lf.lfHeight = textHight;
         hdc = BeginPaint(hWnd, &ps);
         hFont = CreateFontIndirect(&lf);
         hOldFont = (HFONT)SelectObject(hdc, hFont);
 
-        for (int i = 0; i < nLines; i++)
-        {
-            for (int j = 0; j < mRows; j++)
-            {
-                rect = constRect;
-                DrawText(hdc, textTable[i][j].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
-
-                if ((rect.right - rect.left >= maxW))
-                {
-                    maxW = rect.right - rect.left;
-                    posMaxW.y = i;
-                    posMaxW.x = j;
-                }
-
-                if (rect.bottom - rect.top >= maxH)
-                {
-                    maxH = rect.bottom - rect.top;
-                    posMaxH.y = i;
-                    posMaxH.x = j;
-                }
-            }
-        }
-
-        if (maxW <= cellW && maxH <= cellH)
-        {
-            while (maxW <= cellW && maxH <= cellH)
-            {
-                lf.lfHeight++;
-                hFont = CreateFontIndirect(&lf);
-                DeleteObject(SelectObject(hdc, hFont));
-
-                rect = constRect;
-                DrawText(hdc, textTable[posMaxH.y][posMaxH.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
-                maxH = rect.bottom - rect.top;
-
-                rect = constRect;
-                DrawText(hdc, textTable[posMaxW.y][posMaxW.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
-                maxW = rect.right - rect.left;
-            }
-            lf.lfHeight--;
-        }
-        else
-        {
-            while ((maxW > cellW || maxH > cellH) && lf.lfHeight > 1)
-            {
-                lf.lfHeight--;
-                hFont = CreateFontIndirect(&lf);
-                DeleteObject(SelectObject(hdc, hFont));
-
-                rect = constRect;
-                DrawText(hdc, textTable[posMaxH.y][posMaxH.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
-                maxH = rect.bottom - rect.top;
-
-                rect = constRect;
-                DrawText(hdc, textTable[posMaxW.y][posMaxW.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
-                maxW = rect.right - rect.left;
-            }
-        }
+        FindBiggestCell(hdc, text_table, nRows, mColumns, posMaxH, posMaxW, lf);
+        
         hFont = CreateFontIndirect(&lf);
         DeleteObject(SelectObject(hdc, hFont));
 
-        for (int i = 0; i < nLines; i++)
-        {
-            x = 0;
-            for (int j = 0; j < mRows; j++)
-            {
-                rect.left = x;
-                rect.right = x + cellW;
-                rect.top = y;
-                rect.bottom = y + cellH;
-                DrawText(hdc, textTable[i][j].c_str(), -1, &rect, DT_CENTER | DT_WORDBREAK);
-
-                x += cellW;
-            }
-            y += cellH;
-        }
-
-        int currY = TOOL_HIGHT;
-        for (int i = 0; i < nLines; i++)
-        {
-            DrawLine(hdc, 0, currY, wndW, currY);
-            currY += cellH;
-        }
-        DrawLine(hdc, 0, wndH, wndW, wndH);
-
-        int currX = cellW;
-        for (int i = 1; i < mRows; i++)
-        {
-            DrawLine(hdc, currX, TOOL_HIGHT, currX, wndH);
-            currX += cellW;
-        }
-        DrawLine(hdc, wndW, 0, wndW, wndH);
+        DisplayText(hdc, text_table, nRows, mColumns);
+        DisplayTableLines(hdc);
 
         SelectObject(hdc, hOldFont);
-        DeleteObject(hOldFont);
         DeleteObject(hFont);
         EndPaint(hWnd, &ps);
         break;
@@ -219,8 +162,182 @@ BOOL DrawLine(HDC hdc, int x1, int y1, int x2, int y2)
     return LineTo(hdc, x2, y2);
 }
 
+// Вычисление размеров клеток
 void RecalculateTable()
 {
-    cellH = (wndH - TOOL_HIGHT) / nLines;
-    cellW = wndW / mRows;
+    cellH = (wndH - TOOL_BAR_HIGHT) / nRows;
+    cellW = wndW / mColumns;
+}
+
+// Поиск клетки самого большого текста таблицы
+void FindBiggestCell(HDC hdc, std::vector<std::wstring> textTable, int nRows, int mColumns, POINT &posMaxH, POINT &posMaxW, LOGFONT &lf)
+{
+    RECT rect;
+    HFONT hFont;
+    int maxW = 0, maxH = 0;
+
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < mColumns; j++)
+        {
+            rect = constRect;
+            DrawText(hdc, textTable[i * mColumns + j].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
+
+            if ((rect.right - rect.left >= maxW))
+            {
+                maxW = rect.right - rect.left;
+                posMaxW.y = i;
+                posMaxW.x = j;
+            }
+
+            if (rect.bottom - rect.top >= maxH)
+            {
+                maxH = rect.bottom - rect.top;
+                posMaxH.y = i;
+                posMaxH.x = j;
+            }
+        }
+    }
+
+    if (textTable[posMaxH.y * mColumns + posMaxH.x].size() > 0)
+    {
+        if (maxW <= cellW && maxH <= cellH)
+        {
+            while (maxW <= cellW && maxH <= cellH)
+            {
+                lf.lfHeight++;
+                hFont = CreateFontIndirect(&lf);
+                DeleteObject(SelectObject(hdc, hFont));
+
+                rect = constRect;
+                DrawText(hdc, textTable[posMaxH.y * mColumns + posMaxH.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
+                maxH = rect.bottom - rect.top;
+
+                rect = constRect;
+                DrawText(hdc, textTable[posMaxW.y * mColumns + posMaxW.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
+                maxW = rect.right - rect.left;
+            }
+            //lf.lfHeight--;
+        }
+        else
+        {
+            while ((maxW > cellW || maxH > cellH) && lf.lfHeight > 1)
+            {
+                lf.lfHeight--;
+                hFont = CreateFontIndirect(&lf);
+                DeleteObject(SelectObject(hdc, hFont));
+
+                rect = constRect;
+                DrawText(hdc, textTable[posMaxH.y * mColumns + posMaxH.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
+                maxH = rect.bottom - rect.top;
+
+                rect = constRect;
+                DrawText(hdc, textTable[posMaxW.y * mColumns + posMaxW.x].c_str(), -1, &rect, DT_RIGHT | DT_WORDBREAK | DT_CALCRECT);
+                maxW = rect.right - rect.left;
+            }
+        }
+    }
+}
+
+// Рисование текста
+void DisplayText(HDC hdc, std::vector<std::wstring> textTable, int nRows, int mColumns)
+{
+    RECT rect;
+    int x, y = TOOL_BAR_HIGHT;
+
+    for (int i = 0; i < nRows; i++)
+    {
+        x = 0;
+        for (int j = 0; j < mColumns; j++)
+        {
+            rect.left = x;
+            rect.right = x + cellW;
+            rect.top = y;
+            rect.bottom = y + cellH;
+            DrawText(hdc, textTable[i * mColumns + j].c_str(), -1, &rect, DT_CENTER | DT_WORDBREAK);
+
+            x += cellW;
+        }
+        y += cellH;
+    }
+}
+
+void DisplayTableLines(HDC hdc)
+{
+    int currY = TOOL_BAR_HIGHT;
+
+    for (int i = 0; i < nRows; i++)
+    {
+        DrawLine(hdc, 0, currY, wndW, currY);
+        currY += cellH;
+    }
+    DrawLine(hdc, 0, wndH, wndW, wndH);
+
+    int currX = cellW;
+    for (int i = 1; i < mColumns; i++)
+    {
+        DrawLine(hdc, currX, TOOL_BAR_HIGHT, currX, wndH);
+        currX += cellW;
+    }
+    DrawLine(hdc, wndW, 0, wndW, wndH);
+}
+
+void InitializeMenu(HWND hwnd)
+{
+    HMENU hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+    AppendMenu(hFileMenu, MF_STRING, ON_OPEN_FILE, L"Open");
+    SetMenu(hwnd, hMenu);
+}
+
+void InitializeOFN(HWND hwnd)
+{
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = hwnd;
+    ofn.lpstrFile = filename;
+    ofn.nMaxFile = sizeof(filename);
+    ofn.lpstrFilter = L"*.txt";
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = L".\\Resources";
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+}
+
+int LoadData(wchar_t filename[MAX_FILE_NAME], std::vector<std::wstring> textTable)
+{
+    std::wifstream file(filename);
+    file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+
+	if (!file.is_open()) 
+    {
+		return 1;
+	}
+
+	std::wstring line;
+
+	if (std::getline(file, line)) 
+        nRows = std::stoi(line);
+
+	if (std::getline(file, line)) 
+        mColumns = std::stoi(line);
+
+    text_table.clear();
+
+	while (std::getline(file, line)) 
+    {
+		if (!line.empty()) 
+        {
+			text_table.push_back(line);
+		}
+	}
+
+    for (int i = text_table.size(); i < nRows * mColumns; i++)
+        text_table.push_back(L"");
+
+	file.close();
+
+	return 0;
 }
